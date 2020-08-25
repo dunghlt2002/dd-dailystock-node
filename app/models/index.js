3805f889ec530c592f51f4528eaa8a0dfa32c2e6
TriDung <dunghlt2002@gmail.com>
2020-08-24 22:07:48 -0400

    tot 2

--- app/models/index.js
+++ app/models/index.js
@@ -3,18 +3,8 @@
 const Sequelize = require("sequelize");
 const Pool = require("pg").Pool;
 
-var sequelize = new Sequelize('ddfirstdb', 'postgres', '8_Xaloxalac', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
-// var sequelize = new Sequelize('d2vdr8rjjghnmh', 'ngetumvsyuzvwq', '167de969b402aadbfb8a54bf54a211576e8c05cb556b27e9f7ab352b48c9a693', {
-//   host: 'ec2-54-197-254-117.compute-1.amazonaws.com',
+// var sequelize = new Sequelize('ddfirstdb', 'postgres', '8_Xaloxalac', {
+//   host: 'localhost',
 //   dialect: 'postgres',
 //   pool: {
 //     max: 5,
@@ -22,6 +12,16 @@ var sequelize = new Sequelize('ddfirstdb', 'postgres', '8_Xaloxalac', {
 //     idle: 10000
 //   }
 // });
+
+var sequelize = new Sequelize('d2vdr8rjjghnmh', 'ngetumvsyuzvwq', '167de969b402aadbfb8a54bf54a211576e8c05cb556b27e9f7ab352b48c9a693', {
+  host: 'ec2-54-197-254-117.compute-1.amazonaws.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
 
 // Or you can simply use a connection uri
